66577851522db3ed71d5064145ed8c8c201d9121
Wei Tang <hi@that.world>
2019-04-18 09:57:45 +0200

    Implement mul and add test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 mod utils;
 
 use num_bigint::BigInt;
-use num_traits::Zero;
+use num_traits::{Zero, One};
 use num_integer::Integer;
 
-use core::ops::Add;
+use core::ops::{Add, Mul};
 use core::marker::PhantomData;
 use core::fmt::Debug;
 
@@ -109,6 +109,26 @@ impl<C: Curve> Add for Point<C> {
     }
 }
 
+impl<C: Curve> Mul<BigInt> for Point<C> {
+    type Output = Point<C>;
+
+    fn mul(self, mut other: BigInt) -> Point<C> {
+        assert!(other >= BigInt::zero());
+
+        if other == BigInt::zero() {
+            Self::infinity()
+        } else {
+            let mut ret = self.clone();
+            other -= BigInt::one();
+            while other > BigInt::zero() {
+                ret = ret + self.clone();
+                other -= BigInt::one();
+            }
+            ret
+        }
+    }
+}
+
 /// An elliptic curve, where y^2 = x^3 + a*x + b (mod p).
 pub trait Curve: Clone + Eq + PartialEq + Debug {
     fn p() -> BigInt;
@@ -119,6 +139,7 @@ pub trait Curve: Clone + Eq + PartialEq + Debug {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num_traits::Num;
 
     #[derive(Clone, Eq, PartialEq, Debug)]
     /// Testing curve defined by y^2 = x^3 + 1x + 7
@@ -127,6 +148,16 @@ mod tests {
     impl Curve for TestCurve {
         fn p() -> BigInt { BigInt::from(13u32) }
         fn a() -> BigInt { BigInt::from(1u32) }
+        fn b() -> BigInt { BigInt::from(7u32) }
+    }
+
+    #[derive(Clone, Eq, PartialEq, Debug)]
+    /// secp256k1
+    struct P256K1Curve;
+
+    impl Curve for P256K1Curve {
+        fn p() -> BigInt { BigInt::from_str_radix("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16).unwrap() }
+        fn a() -> BigInt { BigInt::zero() }
         fn b() -> BigInt { BigInt::from(7u32) }
     }
 
@@ -140,5 +171,26 @@ mod tests {
         let p3 = p1 + p2;
 
         assert_eq!(p3.value(), Some((BigInt::from(1u32), BigInt::from(10u32))));
+    }
+
+    #[test]
+    fn secp256k1() {
+        let g = Point::<P256K1Curve>::from(PointValue::Value { x: BigInt::from_str_radix("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16).unwrap(), y: BigInt::from_str_radix("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 16).unwrap() });
+
+        assert_eq!(g.clone() * BigInt::one(), g);
+        assert_eq!(
+            g.clone() * BigInt::from(2u32),
+            Point::<P256K1Curve>::from(PointValue::Value {
+                x: BigInt::from_str_radix("C6047F9441ED7D6D3045406E95C07CD85C778E4B8CEF3CA7ABAC09B95C709EE5", 16).unwrap(),
+                y: BigInt::from_str_radix("1AE168FEA63DC339A3C58419466CEAEEF7F632653266D0E1236431A950CFE52A", 16).unwrap()
+            })
+        );
+        assert_eq!(
+            g.clone() * BigInt::from(3u32),
+            Point::<P256K1Curve>::from(PointValue::Value {
+                x: BigInt::from_str_radix("F9308A019258C31049344F85F89D5229B531C845836F99B08601F113BCE036F9", 16).unwrap(),
+                y: BigInt::from_str_radix("388F7B0F632DE8140FE337E62A37F3566500A99934C2231B6CB9FD7584B8E672", 16).unwrap()
+            })
+        );
     }
 }
